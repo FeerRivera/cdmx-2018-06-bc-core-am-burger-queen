b2fd8596e224c549d3bca03d50a8bce9e6611d74
FeerRivera <feerkay15@gmail.com>
2018-10-26 12:30:55 -0500

    Updates

---
+++ precache-manifest.653ddb46f421771447ff34d35419a93c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "734f58c71ef08d84f7e2",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.734f58c7.chunk.js"
+  },
+  {
+    "revision": "096f307aa37abe2409ab",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.096f307a.chunk.js"
+  },
+  {
+    "revision": "734f58c71ef08d84f7e2",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.3cf9e951.chunk.css"
+  },
+  {
+    "revision": "3e64a7fc2a4636d07a0eaace7c383139",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
